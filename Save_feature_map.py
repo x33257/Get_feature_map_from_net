949438703d5b4439f38d3509f0fb082c56ae255b
x33257 <54056224+x33257@users.noreply.github.com>
2023-05-16 10:35:03 +0800

    Add files via upload
    Add some new function

--- Save_feature_map.py
+++ Save_feature_map.py
@@ -1,4 +1,6 @@
 import pickle
+import cv2
+import os
 import os.path
 
 
@@ -37,13 +39,37 @@ def Get_name_from_path(path):
     name=y.split('.')
     return name[0]
 
-if __name__=='__main__':
-    path_load='./FeatureMap_MSRS_VGG03_ssd_vi/00095D_vi_VGGnet_after_CNN3.dat'
-    data=Load_result(path_load)
-    for i in data:
-        print(i)
-        print(data[i].shape)
-    # CnnFusNet 融合后的结果是304*304。 只取0：299 即可
 
-    # path='/home/host/workspace_sda/workpace_all/python_workspace/Net_Base/test_CNN.py'
-    # print(Get_name_from_path(path))
+def Save_FeatureMap_image(feature_map,layername:str):
+    feature_size=feature_map.shape  #B,C,H,W  in most decase B==1
+    img = tensor_to_mat(feature_map)
+    for batch in range(feature_size[0]):
+        save_dir = rf'./FeatureMap_Image/batch{batch}/{layername}'
+        os.makedirs(save_dir, exist_ok=True)
+        for n in range(feature_size[1]):
+            image_name = rf'{save_dir}/{n}.jpg'
+            cv2.imwrite(image_name,img[:,:,n])
+
+def tensor_to_mat(img_tensor):
+    img_tensor1=img_tensor.cpu().permute(0,2,3,1).contiguous().detach().numpy()  #to N*C*H*W
+    size_np=img_tensor1.shape
+    img_mat=(img_tensor1[0:,:,:]*255).reshape(size_np[1],size_np[2],size_np[3]).astype('uint8')
+    return img_mat
+
+
+def Save_image_from_dat(dat_path):
+    data_in_dat=Load_result(dat_path)
+    print('Get the data now! They are:')
+    for i in data_in_dat:
+        print(i)
+        print(data_in_dat[i].shape)
+        Save_FeatureMap_image(data_in_dat[i],i)
+
+
+
+
+if __name__=='__main__':
+    path_load='/home/host/data/test02/Feature_map_dat'+'/FeatureMap_MSRS_VGG03_ssd_ir/00095D_ir_VGGnet_after_CNN3.dat'
+    Save_image_from_dat(path_load)
+
+
